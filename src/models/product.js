c16ec7796f079a6812d57774d5de89839666f3fd
Nicole Moyo <n.moyo@alustudent.com>
2023-01-21 12:06:43 +0200

    add migrations

---
+++ src/models/product.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ProductStore = void 0;
+const database_1 = __importDefault(require("../database"));
+class ProductStore {
+    async index() {
+        try {
+            const dbConn = await database_1.default.connect();
+            const query = 'SELECT * FROM employee';
+            const data = await dbConn.query(query);
+            dbConn.release();
+            console.log(data.rows);
+            return data.rows;
+        }
+        catch (error) {
+            throw new Error(`Cannot retrieve employees ${error}`);
+        }
+    }
+}
+exports.ProductStore = ProductStore;
+const productsql = new ProductStore;
+productsql.index();
--- src/models/product.ts
+++ src/models/product.ts
@@ -10,13 +10,17 @@ export class ProductStore {
     async index(): Promise<Product[]> {
         try {
             const dbConn = await Client.connect();
-            const query = 'SELECT * FROM products';
+            const query = 'SELECT * FROM employee';
             const data = await dbConn.query(query);
             dbConn.release();
+            console.log(data.rows);
             return data.rows;
         } catch (error) {
-            throw new Error(`Cannot retrieve products ${error}`);
+            throw new Error(`Cannot retrieve employees ${error}`);
             
         }
     }
 }
+
+const productsql = new ProductStore;
+productsql.index()
